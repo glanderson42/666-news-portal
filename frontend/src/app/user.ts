152661913fc2fbc35569e8beade9634d2ff5c679
glanderson42 <glanderson@protonmail.com>
2019-12-07 17:21:50 +0100

    create role enum and user interface

--- frontend/src/app/user.ts
+++ frontend/src/app/user.ts
@@ -1,2 +1,10 @@
+import { Role } from './role';
+
 export interface User {
+    id: Number;
+    username: String;
+    password: String;
+    firstName: String;
+    lastName: String;
+    role: Role;
 }
