7537efcde9a804ea1c704962cac24d5dc960edf9
glanderson42 <glanderson@protonmail.com>
2019-12-07 17:36:38 +0100

    create article interface

--- frontend/src/app/article.ts
+++ frontend/src/app/article.ts
@@ -1,2 +1,11 @@
+import { User } from './user';
+
 export interface Article {
+    id: Number,
+    name: String,
+    user: User,
+    postDate: Date,
+    modifiedDate: Date,
+    summary: String,
+    content: String
 }
