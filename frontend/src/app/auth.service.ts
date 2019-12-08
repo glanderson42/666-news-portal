5c645d4bfefe98ec71c0a194ff4c211b6b6a69b9
glanderson42 <glanderson@protonmail.com>
2019-12-08 19:00:38 +0100

    add httpoptions for basic auth

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -7,12 +7,20 @@ import { HttpHeaders } from "@angular/common/http";
 })
 export class AuthService {
   private baseUrl = "http://localhost:8080/api";
-
+  
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': 'Basic ' + btoa('username:password')
+    })
+  };
+  
   constructor(private httpClient: HttpClient) { }
 
   login(loginData) {
     return this.httpClient.post(
-      this.baseUrl + "/users/login", 
+      this.baseUrl + "/users/login",
+      this.httpOptions,
       loginData
     );
   }
@@ -20,50 +28,58 @@ export class AuthService {
   registration(regData) {
     return this.httpClient.post(
       this.baseUrl + "/users/register",
+      this.httpOptions,
       regData
     );
   }
 
   getUsers() {
     return this.httpClient.get(
-      this.baseUrl + "/users/"
+      this.baseUrl + "/users/",
+      this.httpOptions
     );
   }
 
   getUser(id: Number) {
     return this.httpClient.get(
-      this.baseUrl + "/users/" + id
+      this.baseUrl + "/users/" + id,
+      this.httpOptions
     );
   }
 
   updateUser(userData, id: Number) {
     return this.httpClient.put(
       this.baseUrl + "/users/" + id,
+      this.httpOptions,
       userData
     );
   }
 
   deleteUser(id: Number) {
     return this.httpClient.delete(
-      this.baseUrl + "/users/" + id
+      this.baseUrl + "/users/" + id,
+      this.httpOptions
     );
   }
 
   getArticles() {
     return this.httpClient.get(
-      this.baseUrl + "/article"
+      this.baseUrl + "/article",
+      this.httpOptions
     );
   }
 
   getArticle(id: Number) {
     return this.httpClient.get(
-      this.baseUrl + "/article/" + id
+      this.baseUrl + "/article/" + id,
+      this.httpOptions
     );
   }
 
   createArticle(articleData) {
     return this.httpClient.post(
       this.baseUrl + "/article",
+      this.httpOptions,
       articleData
     );
   }
@@ -71,25 +87,29 @@ export class AuthService {
   updateArticle(articleData, id: Number) {
     return this.httpClient.put(
       this.baseUrl + "/article/" + id,
+      this.httpOptions,
       articleData
     );
   }
 
   deleteArticle(id: Number) {
     return this.httpClient.delete(
-      this.baseUrl + "/article/" + id
+      this.baseUrl + "/article/" + id,
+      this.httpOptions
     );
   }
 
    getThumbnails() {
      return this.httpClient.get(
-      this.baseUrl + "/thumbnails"
+      this.baseUrl + "/thumbnails",
+      this.httpOptions
      );
    }
 
    createThumbnail(thumbnailData) {
      return this.httpClient.post(
       this.baseUrl + "/thumbnails/",
+      this.httpOptions,
       thumbnailData
      );
    }
